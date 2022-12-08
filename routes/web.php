0be60b91c7ac26197899998a042fdc4dcb47c41f
mnurhad <nurhadi@nominavipindonesia.com>
2022-12-08 16:59:56 +0700

    Config Redis

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Redis;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/redis', function () {
+    $p = Redis::incr('p');
+    return $p;
 });
